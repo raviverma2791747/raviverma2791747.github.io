ff116d54482f887860b306830ddb11b0dbe53d0c
Ravi Verma <55842522+raviverma2791747@users.noreply.github.com>
2022-01-28 01:00:22 +0530

    Initialized

---
+++ src/page/Certificates.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function Certificates() {
+  return <Container>hh</Container>;
+}
---
+++ src/page/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Home() {
+  return <Container>
+      Holassss
+  </Container>;
+}
+
+export default Home;
